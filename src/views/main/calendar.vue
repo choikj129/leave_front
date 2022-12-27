27f8f5f2311b71dd5396b404b576bb5e92259acf
choikj129 <87593382+choikj129@users.noreply.github.com>
2022-12-27 18:00:06 +0900

    feat : 달력 기능 추가

--- src/views/main/calendar.vue
+++ src/views/main/calendar.vue
@@ -20,57 +20,65 @@
                         {{ calendarTitle }}
                     </v-toolbar-title>
                     <v-spacer></v-spacer>
-                    <v-menu bottom right>
-                        <template v-slot:activator="{ on, attrs }">
-                            <v-btn outlined color="grey darken-2" v-bind="attrs" v-on="on">
-                                <span>{{ typeToLabel[type] }}</span>
-                                <v-icon right>
-                                    mdi-menu-down
-                                </v-icon>
-                            </v-btn>
-                        </template>
-                        <v-list>
-                            <v-list-item @click="type = 'day'">
-                                <v-list-item-title>Day</v-list-item-title>
-                            </v-list-item>
-                            <v-list-item @click="type = 'week'">
-                                <v-list-item-title>Week</v-list-item-title>
-                            </v-list-item>
-                            <v-list-item @click="type = 'month'">
-                                <v-list-item-title>Month</v-list-item-title>
-                            </v-list-item>
-                            <v-list-item @click="type = '4day'">
-                                <v-list-item-title>4 days</v-list-item-title>
-                            </v-list-item>
-                        </v-list>
-                    </v-menu>
+                    
                 </v-toolbar>
             </v-sheet>
             <v-sheet height="600">
-                <v-calendar ref="calendar" v-model="focus" color="primary" :events="events" :event-color="getEventColor"
-                    :type="type" @click:event="showEvent" @click:more="viewDay" @change="updateRange" locale="ko"
-                    :show-month-on-first="false" :day-format="getFormat"></v-calendar>
-                <v-menu v-model="selectedOpen" :close-on-content-click="false" :activator="selectedElement" offset-x>
+                <v-calendar 
+                    ref="calendar"
+                    v-model="focus"
+                    color="secondary"
+                    :events="events"
+                    :event-color="getEventColor"
+                    :type="'month'"                    
+                    @click:date="selectEvent"
+                    @click:event="showEvent"
+                    @change="updateRange"
+                    locale="ko"
+                    :show-month-on-first="false"
+                    :day-format="getFormat"
+                >
+                </v-calendar>
+                <v-menu 
+                    v-model="selectedOpen"
+                    :close-on-content-click="false"
+                    :activator="selectedElement"
+                    offset-x>
                     <v-card color="grey lighten-4" min-width="350px" flat>
                         <v-toolbar :color="selectedEvent.color" dark>
-                            <v-btn icon>
-                                <v-icon>mdi-pencil</v-icon>
-                            </v-btn>
-                            <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+                            <v-menu bottom right>
+                                <template v-slot:activator="{ on, attrs }">
+                                    <v-btn  outlined color="#f5f5f5" v-bind="attrs" v-on="on">
+                                        <span>{{ typeToLabel[type] }}</span>
+                                        <v-icon right>mdi-menu-down</v-icon>
+                                    </v-btn>
+                                </template>
+                                <v-list>
+                                    <v-list-item @click="type = '휴가'">
+                                        <v-list-item-title>휴가</v-list-item-title>
+                                    </v-list-item>
+                                    <v-list-item @click="type = '오전반차'">
+                                        <v-list-item-title>오전 반차</v-list-item-title>
+                                    </v-list-item>
+                                    <v-list-item @click="type = '오후반차'">
+                                        <v-list-item-title>오후 반차</v-list-item-title>
+                                    </v-list-item>
+                                    <v-list-item @click="type = '특별휴가'">
+                                        <v-list-item-title>특별 휴가</v-list-item-title>
+                                    </v-list-item>
+                                </v-list>
+                            </v-menu>
                             <v-spacer></v-spacer>
-                            <v-btn icon>
-                                <v-icon>mdi-heart</v-icon>
-                            </v-btn>
-                            <v-btn icon>
-                                <v-icon>mdi-dots-vertical</v-icon>
+                            <v-btn icon @click="deleteEvent">
+                                <v-icon>mdi-delete</v-icon>
                             </v-btn>
                         </v-toolbar>
                         <v-card-text>
-                            <span v-html="selectedEvent.details"></span>
+                            <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>                            
                         </v-card-text>
                         <v-card-actions>
-                            <v-btn text color="secondary" @click="selectedOpen = false">
-                                Cancel
+                            <v-btn text color="secondary" @click="closeEvent">
+                                close
                             </v-btn>
                         </v-card-actions>
                     </v-card>
@@ -83,23 +91,36 @@
 <script>
 export default {
     data: () => ({
-        focus: '',
-        type: 'month',
+        focus: "",
+        type: "휴가",
         typeToLabel: {
-            month: 'Month',
-            week: 'Week',
-            day: 'Day',
-            '4day': '4 Days',
+            휴가: "휴가",
+            오전반차: "오전 반차",
+            오후반차: "오후 반차",
+            특별휴가: "특별 휴가",
         },
+        testColor : "black red black",
         selectedEvent: {},
         selectedElement: null,
         selectedOpen: false,
+        selectDate : false,
+        startDate : null,
         events: [],
-        colors: ['blue', 'indigo', 'deep-purple', 'cyan', 'green', 'orange', 'grey darken-1'],
-        names: ['Meeting', 'Holiday', 'PTO', 'Travel', 'Event', 'Birthday', 'Conference', 'Party'],
+        colors: ["blue", "indigo", "deep-purple", "cyan", "green", "orange", "grey darken-1"],
+        names: ["Meeting", "Holiday", "PTO", "Travel", "Event", "Birthday", "Conference", "Party"],
         calendarTitle: "",
-        weekly: ["월", "화", "수", "목", "금", "토", "일"]
+        week: ["일", "월", "화", "수", "목", "금", "토"],
+        timeFormat : "T00:00:00"
     }),
+    created() {
+        this.events.push({
+            name: "test",
+            start: new Date(`2022-12-23${this.timeFormat}`),
+            end: new Date(`2022-12-24${this.timeFormat}`),
+            color: this.colors[this.rnd(0, this.colors.length - 1)],
+            index : Math.random().toString(36).substring(2),
+        })
+    },
     mounted() {
         this.calendarTitle = this.$refs.calendar.title
         this.$refs.calendar.checkChange()
@@ -110,13 +131,13 @@ export default {
         },
         viewDay({ date }) {
             this.focus = date
-            this.type = 'day'
+            this.type = "day"
         },
         getEventColor(event) {
             return event.color
         },
         setToday() {
-            this.focus = ''
+            this.focus = ""
         },
         prev() {
             this.$refs.calendar.prev()
@@ -124,53 +145,59 @@ export default {
         next() {
             this.$refs.calendar.next()
         },
-        showEvent({ nativeEvent, event }) {
+        showEvent({nativeEvent, event}){
             const open = () => {
                 this.selectedEvent = event
                 this.selectedElement = nativeEvent.target
                 requestAnimationFrame(() => requestAnimationFrame(() => this.selectedOpen = true))
             }
-
             if (this.selectedOpen) {
                 this.selectedOpen = false
                 requestAnimationFrame(() => requestAnimationFrame(() => open()))
             } else {
                 open()
             }
+            nativeEvent.stopPropagation()
+        },
+        selectEvent(event) {            
+            if (!this.selectDate){
+                this.selectDate = true
+                this.startDate = new Date(`${event.date+this.timeFormat}`)
+                return
+            }
+            const nativeEvent = event.nativeEvent
+            const startDate = `${this.startDate.getFullYear()}-${this.startDate.getMonth()+1}-${this.startDate.getDate()}`
+            const endDate = new Date(`${event.date+this.timeFormat}`)
+            const diffDate = this.startDate < endDate
+
+            const name = startDate == event.date 
+                ? `${startDate} (${this.week[this.startDate.getDay()]}) 휴가` 
+                : `${startDate} (${this.week[this.startDate.getDay()]}) ~ ${event.date} (${this.week[endDate.getDay()]}) 휴가`
+            this.selectedEvent = {
+                name : name,
+                start: diffDate ? this.startDate : endDate,
+                end: diffDate ? endDate : this.startDate,
+                color: this.colors[this.rnd(0, this.colors.length - 1)],
+                index : Math.random().toString(36).substring(2),
+            }
+            this.selectedElement = nativeEvent.target
+            this.selectDate = false
+            this.startDate = null
+                            
+            this.events.push(this.selectedEvent)
 
             nativeEvent.stopPropagation()
         },
+        closeEvent() {                        
+            this.selectedOpen = false
+        },
+        deleteEvent(){
+            this.events = this.events.filter(event => event.index != this.selectedEvent.index)            
+            this.closeEvent()
+            
+        },
         updateRange({ start, end }) {
-            const events = []
-            const min = new Date(`${start.date}T00:00:00`)
-            const max = new Date(`${end.date}T23:59:59`)
-            const days = (max.getTime() - min.getTime()) / 86400000
-            const eventCount = this.rnd(days, days + 20)
-
-            for (let i = 0; i < eventCount; i++) {
-                const allDay = this.rnd(0, 3) === 0
-                const firstTimestamp = this.rnd(min.getTime(), max.getTime())
-                const first = new Date(firstTimestamp - (firstTimestamp % 900000))
-                const secondTimestamp = this.rnd(2, allDay ? 288 : 8) * 900000
-                const second = new Date(first.getTime() + secondTimestamp)
-
-                events.push({
-                    name: this.names[this.rnd(0, this.names.length - 1)],
-                    start: first,
-                    end: second,
-                    color: this.colors[this.rnd(0, this.colors.length - 1)],
-                    timed: !allDay,
-                })
-            }
-            console.log(events)
-            this.events.push({
-                name: "test",
-                start: new Date(`2022-12-23T00:00:00`),
-                end: new Date(`2022-12-24T23:59:59`),
-                color: this.colors[this.rnd(0, this.colors.length - 1)],
-                timed: false,
-            })
-            //   this.events = events
+            // console.log("@@@@@@@@@@@@@@@@")
         },
         rnd(a, b) {
             return Math.floor((b - a + 1) * Math.random()) + a
